736a08683fdccd82d2dd096e174fd3c61b16f2c5
lex0tan <96478989+lex0tan@users.noreply.github.com>
2022-01-17 14:27:20 +0100

    Delete github directory

--- github/com_list.py
+++
@@ -1,6 +0,0 @@
-#Here you can find, and change, all the commands of the bot.
-comm_start = "/start"
-comm_info = "/info"
-comm_listtags = "/listtag"
-comm_vidbytag = "/vidbytag"
-comm_list = (comm_start,comm_info,comm_listtags,comm_vidbytag)
